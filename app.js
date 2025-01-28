c9ec63965c7ed49cf0dd007e2e010e35ff748927
EARNascimento <eduardo90909@outlook.com>
2025-01-28 16:34:30 -0300

    Cria uma array para armazenar nomes e exibe no HTML

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    let amigoDigitado = document.getElementById('amigo').value;
+    amigoDigitado.innerHTML = '';
+
+    if(amigoDigitado == ''){
+        alert("Por favor, insira um nome!");
+        //exibirTextoNaTela('h2', 'Por favor, insira um nome!')
+    } else{
+        amigos.push(amigoDigitado)
+    }
+    limparCampo()
+
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML= "";
+
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+};
+
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function limparCampo() {
+    amigoDigitado = document.getElementById('amigo');
+    amigoDigitado.value = "";
+}
+
